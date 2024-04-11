504b0d026a92e8bd92100c6f5d9d2b8cdb81e7bf
Sagar Maurya <147434812+asmigar@users.noreply.github.com>
2024-04-11 22:08:59 +0530

    ssh command to ssh into backen

--- infra/output.tf
+++ infra/output.tf
@@ -1,6 +1,16 @@
 output "ssh_command_master_node" {
-  value       =  "ssh -i ~/.ssh/${aws_key_pair.this.key_name}.pem ec2-user@${aws_instance.nginx.public_dns}"
+  value       =  "ssh -i ~/.ssh/${aws_key_pair.this.key_name}.pem ec2-user@${aws_instance.nginx.public_dns} ${aws_instance.backend.private_ip}"
   description = "ssh command for connecting to the nginx node"
+}
+
+output "ssh_command_private_instance" {
+  value       = <<-SSHCOMMAND
+  ssh-add -K ~/.ssh/${aws_key_pair.this.key_name}.pem
+  ssh -A ec2-user@${aws_instance.nginx.public_dns}
+  #from ${aws_instance.nginx.public_dns} host
+  ssh ec2-user@${aws_instance.backend.private_ip}
+  SSHCOMMAND
+  description = "ssh command for connecting to the backend instance"
 }
 
 output "access_nginx_here" {
