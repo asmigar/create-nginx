28598930179e6d6d07e84a17b81f54b426242635
Sagar Maurya <147434812+asmigar@users.noreply.github.com>
2025-09-04 20:37:55 +0530

    fix nginx install for amazon linux 2

--- modules/nginx/setup.sh
+++ modules/nginx/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-yum install nginx -y
+sudo amazon-linux-extras install nginx1 -y
 sed -i 's/Welcome to nginx/Welcome to Asmigar/' /usr/share/nginx/html/index.html
 systemctl start nginx
