c03ce39c51965a27b06e7f6f90ac0c94a5d32301
Sagar Maurya <147434812+asmigar@users.noreply.github.com>
2025-09-04 09:43:02 +0530

    remove provisioner to create ssh key file - used local_sensistive_file resource - used pathexpand function to expand '~'

--- modules/nginx/ssh_key_pair.tf
+++ modules/nginx/ssh_key_pair.tf
@@ -6,13 +6,10 @@ resource "tls_private_key" "this" {
 resource "aws_key_pair" "this" {
   key_name   = "${var.ssh_key_name}-${var.env}"
   public_key = tls_private_key.this.public_key_openssh
+}
 
-  provisioner "local-exec" {
-    command = "mkdir -p ~/.ssh; echo '${tls_private_key.this.private_key_openssh}' > ~/.ssh/${self.key_name}.pem; chmod 400 ~/.ssh/${self.key_name}.pem"
-  }
-
-  provisioner "local-exec" {
-    when    = destroy
-    command = "rm -rf ~/.ssh/${self.key_name}.pem"
-  }
+resource "local_sensitive_file" "ssh_private_key" {
+  content  = tls_private_key.this.private_key_openssh
+  filename = pathexpand("~/.ssh/${aws_key_pair.this.key_name}.pem")
+  file_permission = "0400"
 }
